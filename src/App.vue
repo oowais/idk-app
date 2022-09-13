d71c854b9698e67ff6bbb1cf8e3255931beb0f32
Owais <oowais@gmail.com>
2022-09-13 21:51:11 +0200

    refactor

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
   <v-app>
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <v-main> </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
 
-  components: {
-    HelloWorld,
+  data() {
+    return {};
   },
-
-  data () {
-    return {
-      //
-    }
-  },
-})
+});
 </script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
+import vuetify from "vite-plugin-vuetify";
 
-// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-import vuetify from 'vite-plugin-vuetify'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-		vue(),
-		vuetify({ autoImport: true }),
-	],
-  server: {
-    port: 8000,
-  },
+  plugins: [vue(), vuetify({ autoImport: true })],
+  server: { port: 8000 },
 });
